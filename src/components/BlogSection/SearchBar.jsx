2ce3df85c884ff54171ca16dd5fe9b9cbb172e28
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-09-09 12:36:16 +0200

    feat: enhance SearchBar component with reset functionality and improved styling

--- src/components/BlogSection/SearchBar.jsx
+++ src/components/BlogSection/SearchBar.jsx
@@ -2,18 +2,28 @@ import { usePostContext } from '../../contexts/PostContext'
 import './SearchBar.css'
 
 const SearchBar = () => {
-    const { searchQuery, searchPosts } = usePostContext()
+    const { setSearchQuery, searchQuery, searchPosts } = usePostContext()
+
+    function handleReset() {
+        setSearchQuery('')
+    }
+
 
     return (
-        <div className="search-bar">
-            <input
-                type="text"
-                placeholder="Buscar por título, contenido o etiqueta..."
-                value={searchQuery}
-                onChange={(e) => searchPosts(e.target.value)}
-                className="search-input"
-            />
-        </div>
+        <>
+            <div className="search-bar">
+                <input
+                    type="text"
+                    placeholder="Buscar por título, contenido o etiqueta..."
+                    value={searchQuery}
+                    onChange={(e) => searchPosts(e.target.value)}
+                    className="search-input"
+                />
+
+            </div>
+            <p className='bar-reset'
+                onClick={handleReset}>x</p>
+        </>
     )
 }
 
