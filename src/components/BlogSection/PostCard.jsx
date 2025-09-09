9d71bdb7f8b318300412f391176fce6bc46e1106
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-09-09 12:34:32 +0200

    feat: implement PostCard component with styling; refactor PostList to utilize PostCard for rendering posts

--- src/components/BlogSection/PostCard.jsx
+++ src/components/BlogSection/PostCard.jsx
@@ -1,6 +1,141 @@
-const PostCard = () => {
+import "./PostCard.css";
+
+import { useNavigate } from 'react-router-dom'
+import { usePostContext } from '../../contexts/PostContext'
 
 
+
+
+export default function PostCard({ id, image_url, published_at, reading_time, title, headline, tags }) {
+
+
+    // ✅ funzione per convertire il titolo in slug URL-friendly
+    function slugify(string) {
+        return string
+            .toLowerCase()
+            .replace(/[^\w\s-]/g, '')  // rimuove caratteri speciali
+            .trim()
+            .replace(/\s+/g, '-')      // spazi → trattini
+    }
+
+    const { searchPosts } = usePostContext()
+    const navigate = useNavigate()
+
+    const handleOpen = () => {
+        const slug = slugify(title);
+        navigate(`/blog/${id}/${slug}`);
+    };
+
+    const handleTagClick = (e, tag) => {
+        e.stopPropagation();
+        searchPosts(tag);
+    };
+
+
+    return (
+        <article className="post-card"
+            onClick={handleOpen}>
+            <div className="post-card-image">
+                {image_url ?
+                    <div className='post-thumb'>
+                        <img src={image_url} alt="" />
+                    </div> :
+                    null}
+            </div>
+
+            <div className="post-card-content">
+                <div className="post-card-meta">
+                    <span className="date">
+                        {new Date(published_at).toLocaleDateString()}
+                    </span> ··· <span>{reading_time} min</span>
+                </div>
+
+                <h3 className="post-card-title">{title}</h3>
+                <p className="post-card-text">{headline}</p>
+
+                <div className="post-card-tags">
+
+                    {Array.isArray(tags) && tags.length > 0 && (
+                        <>
+
+                            {tags.map((tag, index) => (
+                                <span
+                                    key={index}
+                                    onClick={(e) => {
+                                        e.stopPropagation()
+                                        searchPosts(tag)
+                                    }}
+                                >
+                                    <div className="tag-wrapper">
+
+                                        {tags.map((tag, i) => {
+                                            return (
+                                                <div key={i}
+                                                    className='tag'
+                                                    onClick={(e) => handleTagClick(e, tag)}>
+                                                    {tag}
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                </span>
+                            ))}
+                        </>
+                    )}
+
+                </div>
+            </div>
+        </article>
+    );
 }
 
-export default PostCard
+
+
+
+
+
+
+
+
+
+// <div className="posts-container">
+//     {posts.map(post => (
+//         <div
+//             key={post.id}
+//             className="post-row"
+//             onClick={() => handleClick(post)}
+//         >
+//             {post.image_url ?
+//                 <div className='post-thumb'>
+//                     <img src={post.image_url} alt="" />
+//                 </div> :
+//                 null}
+//             <h3 className="post-title">{post.title}</h3>
+//             <p className="post-headline">{post.headline}</p>
+
+//             <p className="tags">
+//                 {Array.isArray(post.tags) && post.tags.length > 0 && (
+//                     <>
+//                         <span className="tags-label">Etiquetas:</span>{" "}
+//                         {post.tags.map((tag, index) => (
+//                             <span
+//                                 key={index}
+//                                 onClick={(e) => {
+//                                     e.stopPropagation()
+//                                     searchPosts(tag)
+//                                 }}
+//                             >
+//                                 <span className='tag'>
+//                                     {tag}
+//                                     {index < post.tags.length - 1 && ', '}
+//                                 </span>
+//                             </span>
+//                         ))}
+//                     </>
+//                 )}
+//             </p>
+
+//             <p className="date">
+//                 {new Date(post.published_at).toLocaleDateString()}
+//             </p>
+//         </div>
--- src/components/BlogSection/PostList.jsx
+++ src/components/BlogSection/PostList.jsx
@@ -1,52 +1,8 @@
-
-// versione con postdetail
-
 import { useNavigate } from 'react-router-dom'
 import './PostList.css'
 import { usePostContext } from '../../contexts/PostContext'
 import { useState, useEffect } from 'react'
-
-// ✅ alphabet + FlipLetter per effetto “flip”
-const alphabet = "abcdefghijklmnopqrstuvwxyz0123456789 -_".split("")
-
-function FlipLetter({ target, delay }) {
-  const [letter, setLetter] = useState(" ")
-  const [done, setDone] = useState(false)
-
-  useEffect(() => {
-    if (done) return
-
-    let i = 0
-    const interval = setInterval(() => {
-      const random = alphabet[Math.floor(Math.random() * alphabet.length)]
-      setLetter(random)
-      i++
-      if (i > 15) {
-        clearInterval(interval)
-        setLetter(target)
-        setDone(true)
-      }
-    }, delay / 10)
-
-    return () => clearInterval(interval)
-  }, [target, delay, done])
-
-  return (
-    <span style={{ display: "inline-block" }}>
-      {letter}
-    </span>
-  )
-}
-
-// function FlipTag({ text }) {
-//   return (
-//     <span className="tag-badge" style={{ cursor: "pointer" }}>
-//       {text.split("").map((char, i) => (
-//         <FlipLetter key={i} target={char} delay={300 + i * 20} />
-//       ))}
-//     </span>
-//   )
-// }
+import PostCard from './PostCard'
 
 // ✅ funzione per convertire il titolo in slug URL-friendly
 function slugify(title) {
@@ -57,7 +13,7 @@ function slugify(title) {
     .replace(/\s+/g, '-')      // spazi → trattini
 }
 
-// 📄 Componente principale
+
 const PostList = ({ posts }) => {
   const { searchPosts } = usePostContext()
   const navigate = useNavigate()
@@ -73,52 +29,10 @@ const PostList = ({ posts }) => {
 
   return (
     <div className="posts-container">
-      {posts.map(post => (
-        <div
-          key={post.id}
-          className="post-row"
-          onClick={() => handleClick(post)}
-        >
-          {post.image_url ?
-            <div className='post-thumb'>
-              <img src={post.image_url} alt="" />
-            </div> :
-            null}
-
-
-          {/* <div className='post-img-container'>
-            <img src={post.image_url} alt="" />
-          </div> */}
-          <h3 className="post-title">{post.title}</h3>
-          <p className="post-headline">{post.headline}</p>
-
-          <p className="tags">
-            {Array.isArray(post.tags) && post.tags.length > 0 && (
-              <>
-                <span className="tags-label">Etiquetas:</span>{" "}
-                {post.tags.map((tag, index) => (
-                  <span
-                    key={index}
-                    onClick={(e) => {
-                      e.stopPropagation()
-                      searchPosts(tag)
-                    }}
-                  >
-                    <span className='tag'>
-                      {tag}
-                      {index < post.tags.length - 1 && ', '}
-                    </span>
-                  </span>
-                ))}
-              </>
-            )}
-          </p>
-
-          <p className="date">
-            {new Date(post.published_at).toLocaleDateString()}
-          </p>
-        </div>
-      ))}
+      {posts.map((post) => {
+        console.log(post)
+        return <PostCard key={post.id} {...post} />
+      })}
     </div>
   )
 }
