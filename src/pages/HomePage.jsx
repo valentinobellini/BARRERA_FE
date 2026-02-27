dc0a2c9a929cbbb42553315e21e5179fde74e11b
valentinobellini <valentinobellini.dev@gmail.com>
2026-02-27 17:47:02 +0100

    feat: enhance styles for HeroSection, ServiceCard, and index; improve responsiveness and layout adjustments

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ const HomePage = () => {
                             title={
                                 <>
                                     Control de
-                                    <br className="service-title-break" />
+                                    <br className="service-title-break" />{" "}
                                     Hipertensión Arterial
                                 </>
                             }
